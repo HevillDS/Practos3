43be8567a93d632e3af6b315d12a3d387d3dd5ac
HevillDS <hevillds@gmail.com>
2019-02-04 20:30:17 +0300

    PRACTOS3_VAR1

---
+++ migrations/m190203_162601_car.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190203_162601_car
+ */
+class m190203_162601_car extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190203_162601_car cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190203_162601_car cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
